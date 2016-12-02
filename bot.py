b7abe5619e8efad64a7acf047b06bc6b50a3d5b4
Per Liedman <per@liedman.net>
2016-12-02 20:57:44 +0100

    Detect game end and start

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import os
 import sys
 
 
-def format_list(query, header, format):
-    conn = sqlite3.connect(sys.argv[1])
+conn = sqlite3.connect(sys.argv[1])
 
+def format_list(query, header, format):
     c = conn.cursor()
     i = 1
     lines = [header]
@@ -60,15 +60,50 @@ def parse_slack_output(slack_rtm_output):
     return None, None
 
 
+last_score = None
+last_active = None
+is_active = False
+
+
+def check_active():
+    global last_score, last_active, is_active
+    c = conn.cursor()
+    score = c.execute('select sum(score) from rankme').fetchone()[0]
+    now = time.time()
+
+    print 'Score is %d at % .0f' % (score, now)
+
+    if not last_score is None and score != last_score:
+        if not is_active:
+            is_active = True
+            print 'The game has begun'
+            slack_client.api_call("chat.postMessage", channel='#lanparty',
+                                  text='The game is on! :c4:', as_user=True)
+        last_score = score
+        last_active = now
+    elif not last_active is None and now - last_active > 60:
+        is_active = False
+        print 'The game has ended'
+        slack_client.api_call("chat.postMessage", channel='#lanparty',
+                              text='http://giphy.com/gifs/arnold-schwarzenegger-windows-cs-CFjw7eSxjJL8I\n\n' + print_score(), as_user=True)
+
+
 if __name__ == "__main__":
     slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
     READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
 
     if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
+        print("Bot connected and running.")
+        count = 0
         while True:
+            if count % 10 == 0:
+                check_active()
+
+            count += 1
+
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
+                print 'Received "%s" on channel %s' % (command, channel)
                 handle_command(command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
