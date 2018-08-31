eef2a8d56302427d8fb6afe9c58fde456079cef2
Joakim Eliasson <jeliasson@hotmail.com>
2018-08-31 13:42:13 +0200

    Added new skill & team player count compensation

--- pack.py
+++ pack.py
@@ -1,5 +1,6 @@
 def bestPack(items):
     bestDiff = 1e9
+    skillFactor = 1.01
     teams = []
     for x in xrange(1, 2**len(items) / 2 + 1):
         b = 1
@@ -10,14 +11,14 @@ def bestPack(items):
         for n in items:
             if x & b:
                 t1.append(n[0])
-                ts1 += n[1]
+                ts1 += n[1]**skillFactor
             else:
                 t2.append(n[0])
-                ts2 += n[1]
+                ts2 += n[1]**skillFactor
             b = b << 1
-        diff = abs(ts1 - ts2)
+        diff = abs(ts1 - ts2) *  (1 + (abs(len(t1) - len(t2)) / 2))
         teams.append(((t1, t2), diff))
 
     teams.sort(lambda a, b: cmp(a[1], b[1]))
 
-    return teams
+    return teamspython 
