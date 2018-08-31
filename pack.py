f3324046d5a5367f6936ea7e232ed82d631112d9
Joakim Eliasson <jeliasson@hotmail.com>
2018-08-31 13:45:41 +0200

    With math.ceil

--- pack.py
+++ pack.py
@@ -1,3 +1,5 @@
+import math
+
 def bestPack(items):
     bestDiff = 1e9
     skillFactor = 1.01
@@ -16,7 +18,7 @@ def bestPack(items):
                 t2.append(n[0])
                 ts2 += n[1]**skillFactor
             b = b << 1
-        diff = abs(ts1 - ts2) *  (1 + (abs(len(t1) - len(t2)) / 2))
+        diff = abs(ts1 - ts2) *  (1 + math.ceil((abs(len(t1) - len(t2)) / 2)))
         teams.append(((t1, t2), diff))
 
     teams.sort(lambda a, b: cmp(a[1], b[1]))
